81f230d03f5be9aa1a6a1916bdceefdfd42c0e0a
Joao Silva <joao.devweb@gmail.com>
2023-03-25 13:56:17 -0300

    Add jsx-a11y plugin

---
+++ README.md
@@ -0,0 +1,25 @@
+# Joao Silva ESLint config
+
+## Whats included?
+
+- React/Next plugin;
+- React Hooks plugin;
+- JSX a11y plugin;
+- Prettier;
+
+## Setup
+
+1. Install the dependencies
+```
+npm i -D eslint @joaod3v/eslint-config
+```
+
+2. Create a `.eslintrc.json` file extending the config:
+```
+{
+  "extends": "@joaod3v/eslint-config/next"
+  // "extends": "@joaod3v/eslint-config/next"
+}
+```
+
+> You can also use a `.eslintrc.js` instead of JSON if you prefer.
--- next.js
+++ next.js
@@ -1,36 +1,49 @@
 module.exports = {
   env: {
-      browser: true,
-      es2021: true,
-      jest: true,
-      node: true
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true
   },
   extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended'
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-      ecmaFeatures: {
-          jsx: true
-      },
-      ecmaVersion: 12,
-      sourceType: 'module'
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
   },
   plugins: [
-      'react',
-      '@typescript-eslint',
-      'react-hooks'
+    'react',
+    '@typescript-eslint',
+    'react-hooks',
+    'jsx-a11y',
   ],
   rules: {
     'prettier/prettier': ["error", {
-        'trailingComma': 'es5',
-        'semi': true,
-        'singleQuote': true,
-        'endOfLine': 'auto'
-      }],
+      'trailingComma': 'es5',
+      'semi': true,
+      'singleQuote': true,
+      'endOfLine': 'auto'
+    }],
+    'jsx-a11y/alt-text': [
+      'warn',
+      {
+        elements: ['img'],
+        img: ['Image'],
+      },
+    ],
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 'off',
