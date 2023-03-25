4d5921ff7a9db2d1ee88ba561a72fd0cc9454f91
Joao Silva <joao.devweb@gmail.com>
2023-03-25 11:01:12 -0300

    Add configurations file

---
+++ next.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+      browser: true,
+      es2021: true,
+      jest: true,
+      node: true
+  },
+  extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+      ecmaFeatures: {
+          jsx: true
+      },
+      ecmaVersion: 12,
+      sourceType: 'module'
+  },
+  plugins: [
+      'react',
+      '@typescript-eslint',
+      'react-hooks'
+  ],
+  rules: {
+    'prettier/prettier': ["error", {
+        'trailingComma': 'es5',
+        'semi': true,
+        'singleQuote': true,
+        'endOfLine': 'auto'
+      }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/display-name': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+  },
+}
